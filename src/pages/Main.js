32aa763a6fddeae258bf4a760b7c095740534c46
sdrjseka9 <sdrjseka9@naver.com>
2023-02-06 17:47:52 +0900

    0206update

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Mainpage from './Mainpage';
+import Mainpage2 from './Mainpage2';
 
 const Main = () => {
     return (
         <div>
             <Mainpage/>
+            <Mainpage2/>
         </div>
     );
 };
--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -9,31 +9,30 @@ const Mainpage = () => {
     textAlign: 'center',
     margin:"200px"
   };
-
   return (
     <Carousel autoplay>
-    <div>
+    <div className='main_1'>
       <h3 style={contentStyle}>
           <video preload="auto" poster="" muted="muted" autoplay="autoplay" loop="loop">
              <source src="/video/Hit2.mp4" type="video/mp4" />
           </video>
         </h3>
     </div>
-    <div>
+    <div  className='main_1'>
       <h3 style={contentStyle}>
       <video preload="auto" poster="" muted="muted" autoplay="autoplay" loop="loop">
         <source src="/video/ProjectD.mp4" type="video/mp4" />
       </video>
       </h3>
     </div>
-    <div>
+    <div  className='main_1'>
       <h3 style={contentStyle}>
       <video preload="auto" poster="" muted="muted" autoplay="autoplay" loop="loop">
         <source src="/video/BlueArchive.mp4" type="video/mp4" />
       </video>
       </h3>
     </div>
-    <div>
+    <div  className='main_1'>
       <h3 style={contentStyle}>
       <video preload="auto" poster="" muted="muted" autoplay="autoplay" loop="loop">
         <source src="/video/ProjectMagnum.mp4" type="video/mp4" />
@@ -41,35 +40,7 @@ const Mainpage = () => {
       </h3>
     </div>
   </Carousel>
-    //   <div className='main_1'>
-    //     <div className='video1'>
-    //       <video preload="auto" poster="" muted="muted" autoplay="autoplay" loop="loop">
-    //         <source src="/video/Hit2.mp4" type="video/mp4" />
-    //       </video>
-    //     </div>
-    //     <div className='video2'>
-    //       <video preload="auto" poster="" muted="muted" autoplay="autoplay" loop="loop">
-    //         <source src="/video/ProjectD.mp4" type="video/mp4" />
-    //       </video>
-    //     </div>
-    //     <div className='video3'>
-    //       <video preload="auto" poster="" muted="muted" autoplay="autoplay" loop="loop">
-    //         <source src="/video/BlueArchive.mp4" type="video/mp4" />
-    //       </video>
-    //     </div>
-    //     <div className='video4'>
-    //       <video preload="auto" poster="" muted="muted" autoplay="autoplay" loop="loop">
-    //         <source src="/video/ProjectMagnum.mp4" type="video/mp4" />
-    //       </video>
-    //     </div>
-    // </div>
   );
 };
-
-
-
-
-
-
 
 export default Mainpage;
